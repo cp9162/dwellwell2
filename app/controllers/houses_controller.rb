3cfbe2ad4289adf2859b5ddcc7e46691758c1e1c
cp9162 <cpollock@netdesignstudios.com>
2014-10-07 10:55:46 -0400

    added controller for house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -69,6 +69,6 @@ class HousesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def house_params
-      params[:house]
+      params.require(:house).permit(:name, :address, :city, :state, :unit, :zip)
     end
 end
