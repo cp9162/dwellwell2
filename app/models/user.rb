7d749df4f26dd3033e259527fce4b6bf8be3b242
anschang <anschang@hotmail.com>
2014-10-07 13:57:47 -0400

    fixed devise sign in error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  ## mongo db devise error fix
+  include ZeroOidFix
   ## Database authenticatable
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
