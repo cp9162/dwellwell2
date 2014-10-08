0e6a0ad4af66ab0b67bf81074db9f93b1d5c484e
dkim6002 <Danielkim629@gmail.com>
2014-10-08 10:44:37 -0400

    fix 2 for signing redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
  
   def after_sign_in_path_for(source)
-  	return request.env['omniauth.origin'] || house_path(current_user['house_key']) || houses_path
+  	if signed_in?
+  		house_path(current_user['house_key'])
+  	else
+  		return request.env['omniauth.origin'] || stored_location_for(resource) || houses_path
+  	end
   end 
  
 
