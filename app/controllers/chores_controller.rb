c2b1d0651d244a64f6415dd2929c41984354ccae
alienk703 <alienk703@gmail.com>
2014-10-08 13:35:45 -0400

    touched chores

---
+++ app/controllers/chores_controller.rb
@@ -0,0 +1,78 @@
+class ChoresController < ApplicationController
+
+  # GET /chores
+  # GET /chores.json
+def index
+	@chores = Chore.all
+end
+
+  # GET /chores/1
+  # GET /chores/1.json
+def show
+	@chore = Chore.find(params[:id])
+	@user = User.find(current_user)
+	@user.set(chore_key: @chore.id)
+end
+
+  # GET /houses/new
+def new
+	@chore = Chore.new
+end
+
+  # GET /houses/1/edit
+def edit
+end
+
+
+  # POST /houses
+  # POST /houses.json
+  def create
+    @chore = Chore.new(chore_params)
+
+    respond_to do |format|
+      if @chore.save
+        format.html { redirect_to @chore, notice: 'chore was successfully created.' }
+        format.json { render :show, status: :created, location: @chore }
+      else
+        format.html { render :new }
+        format.json { render json: @chore.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /chores/1
+  # PATCH/PUT /chores/1.json
+  def update
+    respond_to do |format|
+      if @chore.update(chore_params)
+        format.html { redirect_to @chore, notice: 'Chore was successfully updated.' }
+        format.json { render :show, status: :ok, location: @chore }
+      else
+        format.html { render :edit }
+        format.json { render json: @chore.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /chores/1
+  # DELETE /chores/1.json
+  def destroy
+    @chore.destroy
+    respond_to do |format|
+      format.html { redirect_to chores_url, notice: 'Chore was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_chore
+      @chore = Chore.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def chore_params
+      params.require(:chore).permit(:title, :is_dont, :city, :assigment)
+    end
+end
+
