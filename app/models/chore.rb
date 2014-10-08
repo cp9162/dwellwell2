e1e52b54e5e0a2acdcc112d2a631acf84352d5b2
anschang <anschang@hotmail.com>
2014-10-08 10:31:50 -0400

    working on chores model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,9 +1,17 @@
 class Chore
   include Mongoid::Document
-  field :title, type: String
-  field :status, type: Boolean
-  field :assignment, type: String
+  field :title, :type => String
+  field :is_done, :type => Boolean
+  field :assignment, :type => String
+  field :completed_on, :type => Datetime
+  field :timestamps
+
+  # do we need a separate timestamp
+
+  has_many_belongs_to_many :users
+  # see http://stackoverflow.com/questions/7000605/how-to-implement-has-many-through-relationships-with-mongoid-and-mongodb
+  # belongs_to :user, through :house
+  # has_many :user, thrugh :house ?
 
   # add validations
-  belongs_to :user, through :house
 end
