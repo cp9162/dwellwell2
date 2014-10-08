ef5af45915d4aa7576412418554e1c540aa8bfa2
anschang <anschang@hotmail.com>
2014-10-08 10:41:36 -0400

    finalized chore model and ready to push

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -9,6 +9,8 @@ class Chore
   # do we need a separate timestamp
 
   has_many_belongs_to_many :users
+  belongs_to :house
+
   # see http://stackoverflow.com/questions/7000605/how-to-implement-has-many-through-relationships-with-mongoid-and-mongodb
   # belongs_to :user, through :house
   # has_many :user, thrugh :house ?
