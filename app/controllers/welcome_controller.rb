f7941fc8269980b619cdcecdb3f6c025fdca4aa0
dkim6002 <Danielkim629@gmail.com>
2014-10-08 09:34:01 -0400

    chore link to added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		if signed_in?
-			redirect_to('/houses/'+ current_user['house_key'])
+			redirect('/houses/'+ current_user['house_key'])
 		end
 	end
 end
