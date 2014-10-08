30402cf4146f28fc74a356fd8cf6b3308f912422
ecgeiser <erica.geiser@gmail.com>
2014-10-08 13:38:31 -0400

    fixed redirect incognito bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
-		if current_user.house_key != nil
+		if current_user == nil
+		elsif current_user.house_key != nil
 			redirect_to ('/houses/' + current_user.house_key)
 		end
 	end
